9df148d8c9281a34dbe24bc1863dd5b0cd1b7e74
Wilson Eduardo Hernández <wilsoneduardohl@gmail.com>
2025-07-24 14:15:31 -0600

    Archivo Education.jsx añadido

---
+++ src/components/Education.jsx
@@ -0,0 +1,25 @@
+export default function Education() {
+  return (
+    <section id="education" className="bg-light py-5 fade-in">
+      <div className="container">
+        <h2 className="mb-4">Formación Académica</h2>
+        <ul className="list-group">
+          <li className="list-group-item">
+            <h5>Educación Primaria</h5>
+            <p className="mb-0">Escuela Oficial Rural Mixta Aldea Platanares</p>
+          </li>
+          <li className="list-group-item">
+            <h5>Educación Básica</h5>
+            <p className="mb-0">
+              Instituto Mixto de Educación Básica por Cooperativa Gregorio Gallardo
+            </p>
+          </li>
+          <li className="list-group-item">
+            <h5>Educación Universitaria</h5>
+            <p className="mb-0">Universidad Mariano Gálvez de Guatemala</p>
+          </li>
+        </ul>
+      </div>
+    </section>
+  )
+}
