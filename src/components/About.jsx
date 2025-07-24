c7e57c5426306c2078ad182ed061dda0d4232d01
Wilson Eduardo Hernández <wilsoneduardohl@gmail.com>
2025-07-24 14:03:39 -0600

    Archivo About añadido

---
+++ src/components/About.jsx
@@ -0,0 +1,16 @@
+export default function About() {
+  return (
+    <section id="about" className="fade-in py-5">
+      <div className="container">
+        <h2 className="mb-4">Sobre mí</h2>
+        <p>
+          Estudiante proactivo de Ingeniería en Sistemas con sólidos conocimientos en desarrollo web, 
+          bases de datos relacionales, redes y control de versiones. Apasionado por el aprendizaje constante, 
+          el diseño de soluciones innovadoras y el desarrollo de software. Me especializo en la creación de interfaces modernas con React, 
+          estructuración de bases de datos SQL y configuración de entornos de red mixtos (Windows/Linux). 
+          Busco una oportunidad para aplicar mis habilidades en proyectos reales que me permitan seguir creciendo profesional y técnicamente.
+        </p>
+      </div>
+    </section>
+  )
+}
