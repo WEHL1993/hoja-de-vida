479d3808c75b9ab95dcb233216e1770ee634a0f1
Wilson Eduardo Hernández <wilsoneduardohl@gmail.com>
2025-07-24 13:55:20 -0600

    Archivo Headr.jsx añadido

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
 
 import './App.css'
+import Navbar from './components/Navbar.jsx'
+import Header from './components/Header.jsx'
+import About from './components/About.jsx'
+import Education from './components/Education.jsx'
+import Experience from './components/Experience.jsx'
+import Skills from './components/Skills.jsx'
+import Projects from './components/Projects.jsx'
+import Contact from './components/Contact.jsx'
+import Footer from './components/Footer.jsx'
+import useScrollAnimation from './hooks/useScrollAnimation.js'
 
 function App() {
+  useScrollAnimation()
 
   return (
-    <div>
-      <h1>Hola Mundo</h1>
-    </div>
+    <>
+      <Navbar />
+      <Header />
+      <About />
+      <Education />
+      <Experience />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+    </>
   )
 }
 
