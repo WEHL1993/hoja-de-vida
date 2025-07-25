7e4987da1353f6724052e9bb8a4b37c216ae0d79
Wilson Eduardo Hernández <wilsoneduardohl@gmail.com>
2025-07-24 18:49:45 -0600

    Header.jsx: edite el Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import profile from '../assets/profile.svg'
+
+export default function Header() {
+  return (
+    <header id="home" className="bg-light text-center pt-5 fade-in">
+      <div className="container py-5">
+        <h1 className="display-4">Wilson Eduardo Hérnandez López</h1>
+        <p className="lead">Desarrollador Full Stack</p>
+      </div>
+    </header>
+  )
+}
