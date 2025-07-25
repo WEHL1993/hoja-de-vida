407b4eb2312c94377b0290458ee5c68044294ed5
Wilson Eduardo Hernández <wilsoneduardohl@gmail.com>
2025-07-24 19:23:51 -0600

    Archivo pie de pagina(Footer.jsx) añadido

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-dark text-white text-center py-3 mt-5">
+      <div className="container">
+        <small>&copy; 2025 Wilson </small>
+      </div>
+    </footer>
+  )
+}
